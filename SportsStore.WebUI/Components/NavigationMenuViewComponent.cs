eba65e1d3f64eb39e9da34bdce59ec05ed85c38c
LeMinh-Quan <aomachx@gmail.com>
2026-07-20 10:52:08 +0700

    Resolve merge conflict

---
+++ SportsStore.WebUI/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SportsStore.WebUI.Components
+{
+    public class NavigationMenuViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var categories = new string[] { "Soccer", "Football", "Chess" };
+            return View(categories);
+        }
+
+
+    }
+}
